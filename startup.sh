48c51c489416311e71af01f2e797def80c954bf2
Syrine Chelly <62653967+SyChell@users.noreply.github.com>
2022-12-01 19:07:25 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
+# optional: for bcp and sqlcmd
+ACCEPT_EULA=Y apt-get install -y mssql-tools18
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
 apt-get install -y unixodbc-dev
-ACCEPT_EULA=Y apt-get install msodbcsql17
 gunicorn --bind=0.0.0.0 --timeout 600 app:app
